53b8cff346a166e4b5f76ffa64ebf7c7b58a8235
Camille <89855408+Kmi1997@users.noreply.github.com>
2023-12-23 12:47:52 +0100

    Fix first bug

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const publicRouter = require("./routes/public.route");
 const adminRouter = require("./routes/admin.route");
 const {urlencoded} = require("express");
 const cookieParser = require('cookie-parser');
-const release = '0.0.0'
+const release = '0.0.1'
 //DB Sync
 db.sequelize.sync().then(() => {
     console.log("DB sync ok");
